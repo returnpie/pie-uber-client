ba1a5305d440ed4a69b1569fd2fcb15e53e1adf8
pie <returnpie@icloud.com>
2020-05-13 00:56:38 +0900

    place

---
+++ src/Routes/Places/PlacesContainer.tsx
@@ -0,0 +1,10 @@
+import React, { useContext } from "react";
+import PlacesPresenter from "./PlacesPresenter";
+import { PlaceContext } from "src/Components/Context/PlaceContext";
+
+const PlacesContainer: React.FC = () => {
+  const { places } = useContext(PlaceContext);
+  return <PlacesPresenter places={places} />;
+};
+
+export default PlacesContainer;
---
+++ src/Routes/Places/PlacesPresenter.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Helmet from "react-helmet";
+import { Link } from "react-router-dom";
+import Header from "src/Components/Header";
+import PlaceTag from "src/Components/PlaceTag";
+import styled from "styled-components";
+import { Place } from "src/types";
+
+const Container = styled.div`
+  padding: 0 40px;
+`;
+
+const SLink = styled(Link)`
+  text-decoration: underline;
+`;
+
+interface IProps {
+  places: Place[];
+}
+
+const PlacesPresenter: React.SFC<IProps> = ({ places }) => (
+  <React.Fragment>
+    <Helmet>
+      <title>Places | Number</title>
+    </Helmet>
+    <Header title={"Places"} backTo={"/"} />
+    <Container>
+      {!places.length && "You have no places"}
+      {places.map((place: Place) => (
+        <PlaceTag
+          key={place.id}
+          id={place.id}
+          isFav={place.isFav}
+          name={place.name}
+          address={place.address}
+        />
+      ))}
+      <SLink to={"/add-place"}>Add some places!</SLink>
+    </Container>
+  </React.Fragment>
+);
+
+export default PlacesPresenter;
