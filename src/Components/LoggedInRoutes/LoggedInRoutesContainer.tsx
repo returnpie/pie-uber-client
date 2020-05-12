0902df65622e94f1c59fe2aa2646bfe82e6ae23c
pie <returnpie@icloud.com>
2020-05-13 00:55:05 +0900

    modify user context, add place context

--- src/Components/LoggedInRoutes/LoggedInRoutesContainer.tsx
+++ src/Components/LoggedInRoutes/LoggedInRoutesContainer.tsx
@@ -1,30 +1,33 @@
 import React, { useContext, useEffect } from "react";
 import LoggedInRoutesPresenter from "./LoggedInRoutesPresenter";
-import { Context } from "../Context/UserContext";
+import { UserContext } from "../Context/UserContext";
+import { PlaceContext } from "../Context/PlaceContext";
 import { useQuery } from "@apollo/react-hooks";
-import { USER_PROFILE } from "src/sharedQueries";
-import { User } from "src/types";
+import { USER_PROFILE, GET_PLACES } from "src/sharedQueries";
+import { User, Place } from "src/types";
 
 const LoggedInRoutesContainer: React.SFC = () => {
-  const context = useContext(Context);
-  const { data } = useQuery(USER_PROFILE);
+  
+  const userContext = useContext(UserContext);
+  const placeContext = useContext(PlaceContext);
+  const userData = useQuery(USER_PROFILE).data;
+  const placeData = useQuery(GET_PLACES).data;
 
   useEffect(() => {
-    if (data && data.GetMyProfile && data.GetMyProfile.ok) {
-      const user = data.GetMyProfile.user as User;
-      const newUser = {
-        id: user.id,
-        profilePhoto: user.profilePhoto,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        email: user.email,
-        isDriving: user.isDriving,
-      };
-      context.setUser(newUser);
+    if (userData && userData.GetMyProfile && userData.GetMyProfile.ok) {
+      const user = userData.GetMyProfile.user as User;
+      userContext.setUser(user);
     }
-  }, [data]);
+  }, [userData]);
 
-  return <LoggedInRoutesPresenter isLoading={data} />;
+  useEffect(() => {
+    if (placeData && placeData.GetMyPlaces && placeData.GetMyPlaces.ok) {
+      const places = placeData.GetMyPlaces.places as Place[];
+      placeContext.setPlaces(places);
+    }
+  }, [placeData]);
+
+  return <LoggedInRoutesPresenter isLoading={userData} />;
 };
 
 export default LoggedInRoutesContainer;
