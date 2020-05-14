65078b6dc76812446cdd2a78fdc824ef490faca2
pie <returnpie@icloud.com>
2020-05-15 01:46:44 +0900

    add address bar

---
+++ src/Components/AddressBar/AddressBar.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.input`
+  position: absolute;
+  background-color: white;
+  border-radius: 5px;
+  -webkit-appearance: none;
+  z-index: 2;
+  width: 80%;
+  border: 0;
+  font-size: 16px;
+  padding: 15px 10px;
+  box-shadow: 0 18px 35px rgba(50, 50, 93, 0.1), 0 8px 15px rgba(0, 0, 0, 0.07);
+  margin: auto;
+  top: 10px;
+  left: 0;
+  right: 0;
+  height: auto;
+`;
+
+interface IProps {
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onBlur: any;
+}
+
+const AddressBar: React.SFC<IProps> = ({ value, onBlur, onChange }) => (
+  <Container
+    value={value}
+    onBlur={onBlur}
+    onSubmit={onBlur}
+    onChange={onChange}
+    placeholder={"Type address"}
+  />
+);
+
+export default AddressBar;
