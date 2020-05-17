92d933fd75faa035984f41592a71f001ee46f2e4
pie <returnpie@icloud.com>
2020-05-18 08:34:50 +0900

    add geoCode

--- src/mapHelpers.ts
+++ src/mapHelpers.ts
@@ -1,11 +1,26 @@
 import axios from "axios";
 import { MAPS_KEY } from "./keys";
 import { toast } from "react-toastify";
-import { Position } from "./types";
+import { Location } from "./types";
 
-export const geoCode = () => {};
+export const geoCode = async (address: string) => {
+  const URL = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${MAPS_KEY}`;
+  const { data } = await axios(URL);
+  if (data && data.results) {
+    const { results } = data;
+    const firstPlace = results[0];
+    const {
+      formatted_address,
+      geometry: { location },
+    } = firstPlace;
+    return { formatted_address, location };
+  } else {
+    toast.error(data.error_message);
+    return false;
+  }
+};
 
-export const reverseGeoCode = async ({ lat, lng }: Position) => {
+export const reverseGeoCode = async ({ lat, lng }: Location) => {
   const URL = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${MAPS_KEY}`;
   const { data } = await axios(URL);
   if (data && data.results) {
@@ -15,6 +30,6 @@ export const reverseGeoCode = async ({ lat, lng }: Position) => {
     return address;
   } else {
     toast.error(data.error_message);
-    return undefined;
+    return false;
   }
 };
