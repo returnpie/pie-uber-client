d241678763dc98cf5fbe1bbcbc1430e975a61ef4
pie <returnpie@icloud.com>
2020-05-31 17:09:51 +0900

    add Message Component

---
+++ src/Components/Message/Message.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+
+interface IContainerProps {
+  mine: boolean;
+}
+
+const Container = styled("div")<IContainerProps>`
+  background-color: ${(props) =>
+    props.mine ? props.theme.blueColor : props.theme.greyColor};
+  color: white;
+  padding: 10px 20px;
+  border-radius: 20px;
+  align-self: ${(props) => (props.mine ? "flex-end" : "flex-start")};
+  border-bottom-right-radius: ${(props) => (props.mine ? "0px" : "20px")};
+  border-bottom-left-radius: ${(props) => (!props.mine ? "0px" : "20px")};
+  margin-bottom: 10px;
+`;
+
+interface IProps {
+  text: string;
+  mine: boolean;
+}
+
+const Message: React.SFC<IProps> = ({ text, mine }) => (
+  <Container mine={mine}>{text}</Container>
+);
+
+export default Message;
