ba66f5db56259f9b6c48caa76b867c012adcf99f
pie <returnpie@icloud.com>
2020-08-13 03:24:58 +0900

    test

--- src/mapHelpers.ts
+++ src/mapHelpers.ts
@@ -6,6 +6,7 @@ import { Location } from "./types";
 export const geoCode = async (address: string) => {
   const URL = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${MAPS_KEY}`;
   const { data } = await axios(URL);
+  console.log(data);
   if (data && data.results) {
     const { results } = data;
     const firstPlace = results[0];
@@ -21,6 +22,7 @@ export const geoCode = async (address: string) => {
 };
 
 export const reverseGeoCode = async ({ lat, lng }: Location) => {
+  console.log(data)''
   const URL = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${MAPS_KEY}`;
   const { data } = await axios(URL);
   if (data && data.results) {
